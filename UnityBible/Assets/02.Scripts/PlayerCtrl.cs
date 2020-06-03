9c7894875f50204aa3a96078d3fb66c88ca5efbe
gooddaeking <gooddaeking@gmail.com>
2020-06-03 17:44:08 +0900

    플레이어 추가 애니메이션 추가

---
+++ UnityBible/Assets/02.Scripts/PlayerCtrl.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class PlayerAnim
+{
+    public AnimationClip idle;
+    public AnimationClip runF;
+    public AnimationClip runB;
+    public AnimationClip runL;
+    public AnimationClip runR;
+}
+
+public class PlayerCtrl : MonoBehaviour
+{
+    private float h = 0.0f;
+    private float v = 0.0f;
+    private float r = 0.0f;
+
+    //접근해야 하는 컴포넌트는 반드시 변수에 할당한 후 사용
+    private Transform tr;
+    //이동 속도 변수(public으로 선언되어 Inspector에 노출됨)
+    public float moveSpeed = 10.0f;
+    public float rotSpeed = 90.0f;
+
+    public PlayerAnim playerAnim;
+    [HideInInspector]
+    public Animation anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+        anim.clip = playerAnim.idle;
+        anim.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+        r = Input.GetAxis("Mouse X");
+
+        Debug.Log("h=" + h.ToString());
+        Debug.Log("v=" + v.ToString());
+
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+
+        tr.Translate(moveDir.normalized * moveSpeed * Time.deltaTime, Space.Self);
+
+        tr.Rotate(Vector3.up * rotSpeed * Time.deltaTime * r);
+
+        if (v >= 0.1f)
+        {
+            anim.CrossFade(playerAnim.runF.name, 0.3f);
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade(playerAnim.runB.name, 0.3f);
+        }
+        else if (h >= 0.1f)
+        {
+            anim.CrossFade(playerAnim.runR.name, 0.3f);
+        }
+        else if (h <= -0.1f)
+        {
+            anim.CrossFade(playerAnim.runL.name, 0.3f);
+        }
+        else
+        {
+            anim.CrossFade(playerAnim.idle.name, 0.3f);
+        }
+    }
+}
